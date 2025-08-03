5c6361c72033e04837f8f813eb35c4dc7fd6cd05
Shikharagrawal-2408 <shikharsatna05@gmail.com>
2025-08-03 20:12:44 +0530

    735.cpp

--- CodeChef/500-800/735.cpp
+++ CodeChef/500-800/735.cpp
@@ -1,169 +1,45 @@
 /*      Cyclic Quadrilateral
-Read problem statements in Mandarin Chinese, Russian, and Vietnamese as well.
-You are given the sizes of angles of a simple quadrilateral (in degrees) 
-A
-A, 
-B
-B, 
-C
-C and 
-D
-D, in some order along its perimeter. Determine whether the quadrilateral is cyclic.
+You are given the sizes of angles of a simple quadrilateral (in degrees) A, B, C and D, in some order along its perimeter. 
+Determine whether the quadrilateral is cyclic.
 
-Note: A quadrilateral is cyclic if and only if the sum of opposite angles is 
-180
-∘
-180 
-∘
- .
+Note: A quadrilateral is cyclic if and only if the sum of opposite angles is 180 degree.
 
 Input
-The first line of the input contains a single integer 
-T
-T denoting the number of test cases. The description of 
-T
-T test cases follows.
-The first and only line of each test case contains four space-separated integers 
-A
-A, 
-B
-B, 
-C
-C and 
-D
-D.
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first and only line of each test case contains four space-separated integers A, B, C and D.
+
 Output
 Print a single line containing the string "YES" if the given quadrilateral is cyclic or "NO" if it is not (without quotes).
 
-You may print each character of the string in uppercase or lowercase (for example, the strings "yEs", "yes", "Yes" and "YES" will all be treated as identical).
-
 Constraints
-1
-≤
-T
-≤
-10
-4
 1≤T≤10 
-4
- 
-1
-≤
-A
-,
-B
-,
-C
-,
-D
-≤
-357
 1≤A,B,C,D≤357
-A
-+
-B
-+
-C
-+
-D
-=
-360
 A+B+C+D=360
-Sample 1:
+
 Input
-Output
 3
 10 20 30 300
 10 20 170 160
 179 1 179 1
+
+Output
 NO
 YES
 NO
-Explanation:
-Example case 1: The sum of two opposite angles 
-A
-+
-C
-=
-10
-∘
-+
-30
-∘
-≠
-180
-∘
-A+C=10 
-∘
- +30 
-∘
- =180 
-∘
- .
 
-Example case 2: The sum of two opposite angles 
-A
-+
-C
-=
-10
-∘
-+
-170
-∘
-=
-180
-∘
-A+C=10 
-∘
- +170 
-∘
- =180 
-∘
-  and 
-B
-+
-D
-=
-20
-∘
-+
-160
-∘
-=
-180
-∘
-B+D=20 
-∘
- +160 
-∘
- =180 
-∘
- .
-
-Example case 3: The sum of two opposite angles 
-B
-+
-D
-=
-1
-∘
-+
-1
-∘
-≠
-180
-∘
-B+D=1 
-∘
- +1 
-∘
- =180 
-∘
- 
+Case 1: The sum of two opposite angles A+C=10 ≠ 180.
+Case 2: The sum of two opposite angles A+C=10 +170 =180 and B+D=20+160 =180 
+Example case 3: The sum of two opposite angles B+D≠180
 */
 /*
-
+In this problem, we are given the angles of a quadrilateral and are asked to determine whether it is cyclic or not. 
+A quadrilateral is considered cyclic if the sum of each pair of opposite angles is equal to 180 degrees. The input consists of multiple 
+test cases, and for each test case, we receive four integers representing the angles of the quadrilateral. According to the properties of 
+cyclic quadrilaterals, if either the sum of angle A and C equals 180 or the sum of angle B and D equals 180, then the quadrilateral can be 
+considered cyclic. In the provided code, the program only checks if the sum of angles A and C is equal to 180, and if so, it prints "YES"; 
+otherwise, it prints "NO". While this may work for the given sample inputs due to the way the angles are arranged, a more robust approach 
+would also check if B + D equals 180, since the opposite angle pairs could be in any order. Nonetheless, the program assumes a fixed order 
+of angles and uses a simple condition to determine the result for each test case.
 */
 #include <bits/stdc++.h>
 using namespace std;
